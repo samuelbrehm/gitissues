a7c76f926c7e82ceb36b464511bd8e02ca6cb3ae
samuelbrehm <samuelbrehm@gmail.com>
2019-02-14 00:32:42 -0200

    Header criado e dependências instaladas e linkadas

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
+import Header from '~/components/Header';
 
 import styles from './styles';
 
 const Repositories = () => (
   <View style={styles.container}>
-    <Text>Repositórios</Text>
+    <Header title="GitIssues" />
   </View>
 );
 
